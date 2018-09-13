88cfde6d902816f1dd30f27201a7b3b4b2b137b6
Yanchao <327889908@qq.com>
2018-09-13 17:22:09 +0800

    access_ip.sh
    access_ip.sh

--- access_ip.sh
+++ access_ip.sh
@@ -13,3 +13,9 @@ cat /var/log/nginx/access.log | awk {'print $1'} | sort -fn | uniq
 echo "##################################"
 echo $date
 #awk '{a[$1]+=1;}END{for(i in a){print a[i]" " i;}}' /var/log/nginx/access.log |sort -g
+
+
+
+
+
+
