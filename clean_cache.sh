0b3973da370dd2c38ffd05d5368496757ddf3142
Yanchao <327889908@qq.com>
2018-09-13 18:11:33 +0800

    auto_clear_cache.sh
    auto_clear_cache.sh

---
+++ clean_cache.sh
@@ -0,0 +1,12 @@
+
+
+#!/bin/bash
+#filename: auto_clear_cache.sh
+#describe:auto clear nginx static files cache
+#author: udax.kr
+#date: 2018/6/22
+
+find /etc/nginx/proxy_cache -type f -delete
+
+
+
